0cbe567b553f3eef0503031213e0f7b0e291ecab
miguel <miguel@gato-note.gato-note.cl>
2016-08-04 16:25:53 -0400

    cargar camiones y revisar el porcentaje de altura

--- php/leerEquipos.php
+++ php/leerEquipos.php
@@ -12,24 +12,29 @@
 
       $result = $conn->query($sql_ultima_lectura);
 
-      $altura_value = "-";
-      foreach ($result as $row) {
-        $altura_value = $row['cp_campo4'];
+      $altura_value = 0;
+      $altura_total = 20000;
+      $porcentaje = 0;
+      foreach ($result as $row1) {
+        $altura_value = $row1['cp_campo4'];
       }
 
+      $altura_value = $altura_value + 0;
+      $porcentaje = ($altura_value * 100) / $altura_total;
 
 
-      $html_camion = "<div class='estado-camion-l'> " .
-                     " <div class='col-lg-6 espacio-ecam-l'> " .
-                     "    <div class='col-lg-3'><img src='img/iconos-menu/bidon-b3.png' class='img-bidon-l'></div> " .
-                     "      <div class='col-lg-9'> " .
-                     "        <div class='div-num-cam-l'><span class='num-cam-l'> ".$row['id_mina']." </span></div> " .
-                     "          <img src='img/iconos-menu/camion-c.png' class='img-camion-l'> " . $altura_value.
-                     "      </div> " .
-                     "  </div>".
-                     "</div>";
-
-      $html_salida .= $html_camion;
+        if($porcentaje < 20){
+          $html_camion =
+                         "<div id='".$row1['cp_id_perfil_cont']."' class='col-md-6 espacio-ecam-l camion-rojo'> " .
+                         "    <div class='col-lg-3'><img src='img/iconos-menu/bidon-b3.png' class='img-bidon-l'></div> " .
+                         "      <div class='col-lg-9'> " .
+                         "        <div class='div-num-cam-l'><span class='num-cam-l'> ".$row['id_mina']." </span></div> " .
+                         "          <img src='img/iconos-menu/camion-c.png' class='img-camion-l' title='altura actual: [".$altura_value."] , ".$porcentaje."% '> " .
+                         "      </div> " .
+                         "  </div>".
+                         "</div>";
+          $html_salida .= $html_camion;
+       }
 
     }
 
