7917792397f651496cc150e04674ff314c34f2cb
miguel <miguel@gato-note.gato-note.cl>
2016-08-03 22:21:14 -0400

    mie 04 agosto

--- php/leerEquipos.php
+++ php/leerEquipos.php
@@ -13,21 +13,24 @@
       $result = $conn->query($sql_ultima_lectura);
 
       $altura_value = "-";
-      foreach ($result as $key => $value) {
-        $altura_value = "$value";
+      foreach ($result as $row) {
+        $altura_value = $row['cp_campo4'];
       }
+
+
 
       $html_camion = "<div class='estado-camion-l'> " .
                      " <div class='col-lg-6 espacio-ecam-l'> " .
                      "    <div class='col-lg-3'><img src='img/iconos-menu/bidon-b3.png' class='img-bidon-l'></div> " .
                      "      <div class='col-lg-9'> " .
-                     "        <div class='div-num-cam-l'><span class='num-cam-l'> ".$altura_value." </span></div> " .
-                     "          <img src='img/iconos-menu/camion-c.png' class='img-camion-l'> " .
+                     "        <div class='div-num-cam-l'><span class='num-cam-l'> ".$row['id_mina']." </span></div> " .
+                     "          <img src='img/iconos-menu/camion-c.png' class='img-camion-l'> " . $altura_value.
                      "      </div> " .
                      "  </div>".
                      "</div>";
 
       $html_salida .= $html_camion;
+
     }
 
     echo $html_salida;
