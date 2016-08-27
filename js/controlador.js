f4901e0fae6b12ca50898abf53ccf32c5447de4b
Pablo Campillay <campillay.pablo@gmail.com>
2016-08-27 00:52:21 -0300

    primer commit pablo

--- js/controlador.js
+++ js/controlador.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-
+		
 		setCategoria(true);
 		loaddatos();
 
-
+		
 	});
 var snom = "";
 var scat = "";
@@ -11,7 +11,7 @@ function loaddatos(){
 	var link = $(location).attr('search');
 	var lscat = "";
 	var lsnom = "";
-
+	
 	if(link != ""){
 		var str = link.split("&");
 		lscat = str[0].replace("?cat=","");
@@ -35,7 +35,7 @@ function loaddatos(){
 			var fecha_final = dia+"/"+mes+" "+hora+":"+min;
 			//console.log(fecha_final);
 			$.get('templates/categorias.mst', function(templates) {
-
+					
 					var templedata = {
 						ide: ide
 					}
@@ -48,7 +48,7 @@ function loaddatos(){
 			});
 		});
     });
-
+	
 	$.getJSON('php/json/equipos.json', function(data){
 		var mensaje="";
 		var temperatura = 0;
@@ -57,7 +57,7 @@ function loaddatos(){
 		var ib = 0;
 		var vb = 0;
 		var il = 0;
-		var vl = 0;
+		var vl = 0;		
 		$.each(data, function(p, v){
 			if(v.Nombre == snom){
 				mensaje = v.Mensaje;
@@ -71,7 +71,7 @@ function loaddatos(){
 			ib = 0;
 			vb = 0;
 			il = 0;
-			vl = 0;
+			vl = 0;		
 
 			if(mensaje.length > 0){
 				//$("#btn-Sitio-"+i).addClass("estado-alerta");
@@ -159,9 +159,9 @@ function loaddatos(){
 			}
 		}
 		$(".subtitulo-fot span").html(Math.round(load)+" %");
+		
 
-
-		setTimeout ('loaddatos()', 5000);
+		setTimeout ('loaddatos()', 5000); 
 	});
 }
 $(".btn-coment").click(function(){
@@ -200,3 +200,4 @@ function comentarios(estado, mensaje, nomequi){
         });
     }
 }
+
--- php/fatfree-master/conn.php
+++
@@ -1,4 +0,0 @@
-<?php
-$f3 = require('lib/base.php');
-$db=new DB\SQL( 'mysql:host=localhost;port=3306;dbname=slxcontrol', 'root', 'mysql' );
-?>
